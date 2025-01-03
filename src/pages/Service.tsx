6fbf511f8c4db81cb948c57f16cd68bbed0957f9
Alekbzh <Alexx.le_gal@live.fr>
2025-01-03 17:26:26 +0100

    Update Service.tsx
    delete markdown

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -2,8 +2,7 @@ import Header from "@/components/Header";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import React from "react";
-import ReactMarkdown from "react-markdown";
+
 
 const Service = () => {
   const { serviceId } = useParams();
@@ -59,102 +58,142 @@ const services = [
   {
     id: "reeducation-abdomino-perineale",
     title: "Rééducation abdomino-périnéale",
-    fullDescription: `
-      ### À qui s’adresse la rééducation périnéale ?
+    fullDescription: (
+      <>
+        <h3>À qui s’adresse la rééducation périnéale ?</h3>
+        <p>
+          La rééducation périnéale s’adresse principalement aux personnes ayant
+          besoin de renforcer ou de restaurer la fonction musculaire du périnée.
+        </p>
+        <ul>
+          <li>Les femmes en post-partum</li>
+          <li>Les femmes en période de ménopause</li>
+          <li>Les personnes souffrant d’incontinence urinaire ou fécale</li>
+          <li>Les hommes après une chirurgie de la prostate</li>
+          <li>Les personnes avec des douleurs pelviennes ou périnéales</li>
+        </ul>
 
-      La rééducation périnéale s’adresse principalement aux personnes ayant besoin de renforcer ou de restaurer la fonction musculaire du périnée.  
-      Elle concerne particulièrement :
-      - Les femmes en post-partum
-      - Les femmes en période de ménopause
-      - Les personnes souffrant d’incontinence urinaire ou fécale
-      - Les hommes après une chirurgie de la prostate
-      - Les personnes avec des douleurs pelviennes ou périnéales
+        <h3>Pourquoi consulter un kinésithérapeute spécialisé :</h3>
+        <ul>
+          <li>Incontinence urinaire</li>
+          <li>Impériosités urinaires et fécales</li>
+          <li>Incontinence anale</li>
+          <li>
+            Troubles de la statique pelvienne (descente d’organe et pesanteur
+            pelvienne)
+          </li>
+          <li>
+            Douleurs pelviennes (dyspareunies, vaginisme, vulvodynies,
+            endométriose)
+          </li>
+          <li>Diastasis des grands droits</li>
+        </ul>
 
-      ### Pourquoi consulter un kinésithérapeute spécialisé :
+        <h3>En quoi consiste la rééducation ?</h3>
+        <p>
+          La rééducation périnéale par un kinésithérapeute vise à renforcer,
+          assouplir ou rééduquer les muscles du plancher pelvien (le périnée),
+          situés entre le pubis et le coccyx. Ces muscles jouent un rôle clé
+          dans :
+        </p>
+        <ul>
+          <li>La continence urinaire et anale</li>
+          <li>Le soutien des organes pelviens</li>
+          <li>La fonction sexuelle</li>
+        </ul>
+        <p>
+          Cette rééducation est particulièrement recommandée après un
+          accouchement, mais elle peut aussi être utile dans d’autres contextes
+          tout au long de la vie d’une femme, comme l’incontinence, les
+          prolapsus ou les douleurs pelviennes.
+        </p>
 
-      - Incontinence urinaire
-      - Impériosités urinaires et fécales
-      - Incontinence anale
-      - Troubles de la statique pelvienne (descente d’organe et pesanteur pelvienne)
-      - Douleurs pelviennes (dyspareunies, vaginisme, vulvodynies, endométriose)
-      - Diastasis des grands droits
-
-      ### En quoi consiste la rééducation ?
-
-      La rééducation périnéale par un kinésithérapeute vise à renforcer, assouplir ou rééduquer les muscles du plancher pelvien (le périnée), situés entre le pubis et le coccyx.  
-      Ces muscles jouent un rôle clé dans :
-      - La continence urinaire et anale
-      - Le soutien des organes pelviens
-      - La fonction sexuelle
-
-      Cette rééducation est particulièrement recommandée après un accouchement, mais elle peut aussi être utile dans d’autres contextes tout au long de la vie d’une femme, comme l’incontinence, les prolapsus ou les douleurs pelviennes.
-
-      ### Déroulement de la rééducation périnéale :
-
-      La rééducation commence par un **bilan** afin d’évaluer la problématique et les objectifs de la patiente. Cela peut inclure :
-      - L’évaluation de symptômes
-      - L’utilisation de questionnaires
-      - Un examen clinique pour évaluer le périnée
-
-      Selon le bilan, il peut être nécessaire d’utiliser une sonde intracavitaire permettant l’utilisation de techniques comme le biofeedback ou l’électrostimulation.  
-      Les autres techniques peuvent inclure :
-      - Massage
-      - Étirements
-      - Techniques respiratoires
-      - Exercices spécifiques permettant le contrôle postural de la continence et du périnée
-    `,
+        <h3>Déroulement de la rééducation périnéale :</h3>
+        <p>
+          La rééducation commence par un <strong>bilan</strong> afin d’évaluer
+          la problématique et les objectifs de la patiente. Cela peut inclure :
+        </p>
+        <ul>
+          <li>L’évaluation de symptômes</li>
+          <li>L’utilisation de questionnaires</li>
+          <li>Un examen clinique pour évaluer le périnée</li>
+        </ul>
+        <p>
+          Selon le bilan, il peut être nécessaire d’utiliser une sonde
+          intracavitaire permettant l’utilisation de techniques comme le
+          biofeedback ou l’électrostimulation. Les autres techniques peuvent
+          inclure :
+        </p>
+        <ul>
+          <li>Massage</li>
+          <li>Étirements</li>
+          <li>Techniques respiratoires</li>
+          <li>
+            Exercices spécifiques permettant le contrôle postural de la
+            continence et du périnée
+          </li>
+        </ul>
+      </>
+    ),
   },
   {
     id: "reeducation-post-partum",
     title: "Rééducation du post-partum",
-    fullDescription: "En cours",
+    fullDescription: <p>En cours</p>,
   },
   {
     id: "soins-bien-etre",
     title: "Soins bien-être",
-    fullDescription: "En cours",
+    fullDescription: <p>En cours</p>,
   },
   {
     id: "yoga",
     title: "Yoga",
-    fullDescription: `
-      ### Ma découverte du yoga et son intégration en kinésithérapie
+    fullDescription: (
+      <>
+        <h3>Ma découverte du yoga et son intégration en kinésithérapie</h3>
+        <p>
+          J’ai découvert le yoga pendant le confinement, une période où j’ai
+          appris l’importance de la discipline nécessaire pour progresser dans
+          cette pratique. Cette découverte m’a conduit à suivre une formation
+          de 200 heures au sein de l’école Amba Yoga. Ce cursus m’a permis
+          d’approfondir ma compréhension du yoga et d’en embrasser une vision
+          plus globale, à la fois philosophique et pratique.
+        </p>
+        <p>
+          Le yoga ne se limite pas à une activité sportive classique. Il s’agit
+          avant tout d’une pratique centrée sur l’écoute de soi, permettant de
+          mieux connaître ses limites et d’apprendre à les respecter.
+        </p>
 
-      J’ai découvert le yoga pendant le confinement, une période où j’ai appris l’importance de la discipline nécessaire pour progresser dans cette pratique.  
-      Cette découverte m’a conduit à suivre une formation de 200 heures au sein de l’école Amba Yoga, où j’ai approfondi ma compréhension du yoga, tant philosophique que pratique.
-
-      Le yoga ne se limite pas à une activité sportive classique. Il s’agit avant tout d’une pratique centrée sur l’écoute de soi, permettant de mieux connaître ses limites et d’apprendre à les respecter.
-
-      ### Yoga et kinésithérapie : une complémentarité évidente
-
-      La kinésithérapie se concentre sur la restauration des fonctions physiques, tandis que le yoga adopte une approche holistique combinant mouvements physiques, respiration, et méditation.  
-      Cette complémentarité est bénéfique pour :
-      - Améliorer la mobilité
-      - Renforcer la souplesse
-      - Gérer la douleur, notamment dans des pathologies chroniques comme l’endométriose
-
-      Certaines postures de yoga s’intègrent parfaitement dans les protocoles de rééducation, offrant aux patients un outil pour se reconnecter à leur corps et accompagner leur guérison.
-    `,
+        <h3>Yoga et kinésithérapie : une complémentarité évidente</h3>
+        <p>
+          La kinésithérapie se concentre sur la restauration des fonctions
+          physiques, tandis que le yoga adopte une approche holistique
+          combinant mouvements physiques, respiration, et méditation. Cette
+          complémentarité est bénéfique pour :
+        </p>
+        <ul>
+          <li>Améliorer la mobilité</li>
+          <li>Renforcer la souplesse</li>
+          <li>
+            Gérer la douleur, notamment dans des pathologies chroniques comme
+            l’endométriose
+          </li>
+        </ul>
+        <p>
+          Certaines postures de yoga s’intègrent parfaitement dans les
+          protocoles de rééducation, offrant aux patients un outil pour se
+          reconnecter à leur corps et accompagner leur guérison.
+        </p>
+      </>
+    ),
   },
   {
     id: "reeducation-cancer-sein",
     title: "Rééducation après le cancer du sein",
-    fullDescription: "En cours",
+    fullDescription: <p>En cours</p>,
   },
 ];
 
-
-const ServiceList = () => {
-  return (
-    <div>
-      {services.map((service) => (
-        <div key={service.id} className="mb-8">
-          <h2 className="text-xl font-bold mb-4">{service.title}</h2>
-          <ReactMarkdown>{service.fullDescription}</ReactMarkdown>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default ServiceList;
+export default Service;
