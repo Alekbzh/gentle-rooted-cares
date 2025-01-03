c932ba323580192f44477c421b10c7f027884ba5
Alekbzh <Alexx.le_gal@live.fr>
2025-01-03 15:42:10 +0100

    Update Service.tsx

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -57,36 +57,8 @@ const services = [
     id: "reeducation-abdomino-perineale",
     title: "Rééducation abdomino-périnéale",
     description: "Accompagnement personnalisé pour la rééducation du périnée, adapté à vos besoins spécifiques.",
-    fullDescription:`<strong>A qui s’adresse la rééducation périnéale ?</strong>
-
-La rééducation périnéale s’adresse principalement aux personnes ayant besoin de renforcer ou de restaurer la fonction musculaire du périnée.
-Elle concerne particulièrement :
-- Les femmes  en post-partum 
-- Les femmes en période de ménopause 
-- Les personnes souffrant d’incontinence urinaire ou fécale 
-- Les hommes après une chirurgie de la prostate 
-- Les personnes avec des douleurs pelviennes ou périnéales
-
-<strong>Pourquoi consulter un kinésithérapeute spécialisé :</strong>
-
-- Incontinence urinaire 
-- Imperiosités urinaires et fécales
-- Incontinence anale
-- Troubles de la statique pelvienne (descente d’organe et pesanteur pelvienne)
-- Douleurs pelviennes (dyspareunies, vaginisme, vulvodynies, endométriose)
-- Diastasis des grands droits
-
-<strong>En quoi consiste la rééducation ?</strong>
-
-La rééducation périnéale par un kinésithérapeute vise à renforcer, assouplir ou rééduquer les muscles du plancher pelvien (le périnée), situés entre le pubis et le coccyx. Ces muscles jouent un rôle clé dans la continence urinaire et anale, le soutien des organes pelviens et la fonction sexuelle. Cette rééducation est particulièrement recommandée après un accouchement, mais elle peut aussi être utile dans d’autres contextes tout au long de la vie d’une femme, comme l’incontinence, les prolapsus ou les douleurs pelviennes.
-
-<strong>Déroulement de la rééducation périnéale :</strong>
-
-La rééducation commence par un bilan afin d’évaluer la problématique et les objectifs de la patiente.
-Cela peut inclure l’évaluation de symptômes, l’utilisation de questionnaires et un examen clinique pour évaluer le périnée.
-Selon le bilan il peut être nécessaire d’utiliser une sonde intracavitaire permettant l’utilisation de techniques comme le biofeedback ou l’electrostimulation. 
-Les autres techniques peuvent être du massage, des étirements, l’utilisation de techniques respiratoires ou d’exercices spécifiques permettant le contrôle postural de la continence et du périnée`,
-},  
+    fullDescription:`En cours`,  
+  },  
   {
     id: "reeducation-post-partum",
     title: "Rééducation du post-partum",
