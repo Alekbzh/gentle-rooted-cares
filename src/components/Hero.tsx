435ce9cbdb2ffb4eab179059891724be1130eb39
Alekbzh <Alexx.le_gal@live.fr>
2025-01-03 16:42:59 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,11 @@ const Hero = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-serif font-medium text-gray-900 mb-8">
-            Kinésithérapeute spécialisée en périnéologie
+            Camille Mazzolini </br>
+            Kinésithérapeute
           </h1>
           <p className="text-xl text-gray-700 mb-12">
-            Expertise et accompagnement personnalisé à Pluneret et Vannes
+            rééducation en pelvi-perineologie
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button
