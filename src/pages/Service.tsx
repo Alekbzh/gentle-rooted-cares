0f9d0a2b26e1710d92eb73647333ca6c2d63a60b
Alekbzh <Alexx.le_gal@live.fr>
2025-01-03 10:40:18 +0100

    Update Service.tsx
    add react markdown

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import { useParams, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import ReactMarkdown from "react-markdown";
 
 const Service = () => {
   const { serviceId } = useParams();
