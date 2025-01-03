c96c16d0e1f54562355b1a227bdff07c96483210
Alekbzh <Alexx.le_gal@live.fr>
2025-01-03 16:45:54 +0100

    Update Hero.tsx
    br

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-serif font-medium text-gray-900 mb-8">
-            Camille Mazzolini </br>
+            Camille Mazzolini <br />
             Kinésithérapeute
           </h1>
           <p className="text-xl text-gray-700 mb-12">
